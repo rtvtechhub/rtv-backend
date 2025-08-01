027956d0c5406b89ff15bd840462ee509a651a80
abdurrakib321 <abdur@zendot.in>
2025-08-01 17:51:34 +0530

    backend repo for rtvnewsnetwork is initialised

---
+++ src/main/java/com/rtvnewsnetwork/RtvnewsnetworkApplication.java
@@ -0,0 +1,13 @@
+package com.rtvnewsnetwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RtvnewsnetworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RtvnewsnetworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rtvnewsnetwork/RtvnewsnetworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rtvnewsnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RtvnewsnetworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
