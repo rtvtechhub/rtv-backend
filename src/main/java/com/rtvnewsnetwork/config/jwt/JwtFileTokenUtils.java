2313bad659c397f5e24260028e4cab69e1ae1aa0
RTV-TECH <tech@rtvnewsnetwork.com>
2025-08-11 16:44:39 +0530

    file upload feature added

---
+++ src/main/java/com/rtvnewsnetwork/config/jwt/JwtFileTokenUtils.java
@@ -0,0 +1,50 @@
+package com.rtvnewsnetwork.config.jwt;
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
+import java.time.Instant;
+import java.util.Base64;
+import java.util.Date;
+import java.util.UUID;
+
+
+
+@Component
+public class JwtFileTokenUtils {
+
+    @Value("${rtv.jwt.lifetime}")
+    private long jwtLifeDuration;
+
+
+    @Value("${rtv.jwt.secret}")
+    private String secretKey;
+
+    @Value("${rtv.refreshToken.secret}")
+    private String refreshTokenSecretKey;
+
+    public String generateToken() {
+        return Jwts.builder()
+                .subject("Kskfjskfaksfjksdf")
+                .id(UUID.randomUUID().toString())
+                .issuedAt(new Date())
+                .expiration(Date.from(Instant.now().plusSeconds(jwtLifeDuration)))
+                .signWith(getKey())
+                .compact();
+    }
+
+
+    public SecretKey getKey() {
+        return new SecretKeySpec(
+                Base64.getDecoder().decode(secretKey),
+                "HmacSHA256"
+        );
+    }
+
+}
+
---
+++ src/main/java/com/rtvnewsnetwork/file/controller/FileProxyController.java
@@ -0,0 +1,32 @@
+package com.rtvnewsnetwork.file.controller;
+
+import com.rtvnewsnetwork.config.model.UploadedFile;
+import com.rtvnewsnetwork.file.model.VideoAndThumbnailResponse;
+import com.rtvnewsnetwork.file.service.FileUploadService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/rtv")
+@RequiredArgsConstructor
+public class FileProxyController {
+
+    private final FileUploadService rtvService;
+
+    @PostMapping("/uploadFile")
+    public ResponseEntity<UploadedFile> uploadFile(
+            @RequestParam("file") MultipartFile file) throws Exception {
+        return ResponseEntity.ok(rtvService.uploadFile(file));
+    }
+
+    @PostMapping("/videoAndThumbnailUpload")
+    public ResponseEntity<VideoAndThumbnailResponse> uploadVideoAndThumbnail(
+            @RequestParam("file") MultipartFile file,
+            @RequestParam(value = "videoPath", defaultValue = "MEDIA_VIDEOS") String videoPath,
+            @RequestParam(value = "thumbnailPath", defaultValue = "MEDIA_IMAGES") String thumbnailPath
+    ) throws Exception {
+        return ResponseEntity.ok(rtvService.uploadVideoAndThumbnail(file, videoPath, thumbnailPath));
+    }
+}
